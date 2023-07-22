ba371df8f1d8873c4e501cc1b865877fe8f91173
Bruno Cardoso de Oliveira <119974445+TheBurno@users.noreply.github.com>
2023-07-21 22:49:29 -0400

    Create sistema.cs

---
+++ sistema.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace CaixaEletronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notasDisponiveis = { 100, 50, 10, 5, 1 };
+
+            Console.Write("Digite o valor do saque: ");
+            int valorSaque = int.Parse(Console.ReadLine());
+
+            if (valorSaque < 10 || valorSaque > 600)
+            {
+                Console.WriteLine("Valor de saque inválido. Valores válidos estão entre 10 e 600.");
+            }
+            else
+            {
+                Console.WriteLine("Quantidade de notas fornecidas:");
+                foreach (int nota in notasDisponiveis)
+                {
+                    int quantidadeNotas = valorSaque / nota;
+                    valorSaque %= nota;
+                    Console.WriteLine($"{quantidadeNotas} nota(s) de {nota} reais");
+                }
+            }
+        }
+    }
+}
